9a5a2f90b85c50022a257ca0955d9305100937bb
Ratan Deep Singh <ratandeep.blr.12@gmail.com>
2023-09-18 13:22:36 +0530

    Revert "cors updated"
    This reverts commit ad81bf79ed029c67981e39da83bc0c7d7ec50f31.

--- server/app.js
+++ server/app.js
@@ -3,25 +3,20 @@ const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
-dotenv.config({ path: './config.env' });
+dotenv.config({path: './config.env'});
 
-const app = express();
+const app=express();
 
-port = process.env.PORT;
+port=process.env.PORT;
 
 require('./db/conn');
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-})
-
-
 app.use(cors());
-app.use(express.json({ limit: '8mb' }));
-app.use(express.urlencoded({ limit: '8mb', extended: true }));
+app.use(express.json({limit: '8mb'}));
+app.use(express.urlencoded({limit: '8mb', extended: true}));
 app.use(cookieParser());
 app.use(require('./router/auth'));
 
-app.listen(port, () => {
+app.listen(port,()=>{
     console.log(`server running at ${port}`);
 })
