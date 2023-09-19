dde9ff274c215bdc69fbc928da0123fec86e479e
Ratan Deep Singh <ratandeep.blr.12@gmail.com>
2023-09-19 13:19:36 +0530

    route controller updated

--- controller/handler.js
+++ controller/handler.js
@@ -1,6 +1,126 @@
-const nodemailer = require('nodemailer');
 const User = require('../db/userSchema');
+const nodemailer = require('nodemailer');
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+
+const signupHandler = async (req, res) => {
+    const { name, email, phone, password, cpassword, profileImage } = req.body;
+
+    if (!name || !email || !phone || !password || !cpassword || !profileImage) {
+        return res.status(422).json({ message: "pls fill the form" });
+    }
+    if (password != cpassword) {
+        return res.status(423).json({ message: "password does not match" });
+    }
+    User.findOne({ phone: phone }).then(
+        (userExist) => {
+            if (userExist) {
+                return res.status(424).json({ message: "user already exist" });
+            }
+
+            const user = new User({ name, email, phone, password, profileImage });
+
+            user.save().then(() => {
+                res.status(201).json({ message: "user registered successfully" })
+            }).catch(err => { res.status(500).json({ message: "Password must have atleast 8 characters" }) })
+        }
+    ).catch(err => { console.log(err) });
+}
+
+
+const loginHandler = async (req, res) => {
+    const { phone, password } = req.body;
+    if (!phone) {
+        return res.status(424).json({ message: "empty username field" });
+    }
+    if (!password) {
+        return res.status(423).json({ message: "empty password field" });
+    }
+    User.findOne({ phone: phone }).then(
+        (userExist) => {
+            bcrypt.compare(password, userExist.password, (err, result) => {
+                if (result) {
+                    userExist.generateAuthToken().then(function (token) {
+                        res.cookie('jwtoken', token, {
+                            expires: new Date(Date.now() + 86400000),
+                            httpOnly: true
+                        }).status(201).json({ message: "login successfully" });
+                    });
+                }
+                else {
+                    res.status(401).json({ message: "incorrect password" })
+                }
+            })
+        }
+    ).catch(err => { res.status(422).json({ message: "user does not exist" }); })
+}
+
+
+const logoutHandler = async (req, res) => {
+    req.rootUser.tokens = req.rootUser.tokens.filter((item) => {
+        return item.token !== req.token
+    })
+    req.rootUser.save().then(() => {
+        res.status(202).clearCookie('jwtoken').json({message:'cookie cleared'});
+    })
+}
+
+
+const deleteAcc = async (req, res) => {
+    const { pass } = req.body;
+    bcrypt.compare(pass, req.rootUser.password, (err, result) => {
+        if (result) {
+            User.findOneAndDelete({ _id: req.user_id }).then(() => {
+                res.clearCookie('jwtoken', { path: '/' });
+                res.status(200).json({ message: 'Account Deleted' });
+            })
+        }
+        else {
+            res.status(500).json({ message: 'incorrect password' });
+        }
+    });
+}
+
+
+const verifyPass = async (req, res) => {
+    const { pass } = req.body;
+    bcrypt.compare(pass, req.rootUser.password, (err, result) => {
+        if (result) {
+            res.status(200).send(req.rootUser);
+        }
+        else {
+            res.status(500).json({ message: 'incorrect password' });
+        }
+    });
+}
+
+
+const editProfile = async (req, res) => {
+    const { _id, name, email, phone, profileImage } = req.body;
+    User.findOne({ _id: _id }).then(
+        (userExist) => {
+            User.findOne({ phone: phone }).then((phoneExist) => {
+
+                if (phoneExist && phoneExist.phone !== userExist.phone) {
+                    res.status(501).json({ message: "phone number is already registered" });
+                }
+                else {
+                    userExist.name = name;
+                    userExist.email = email;
+                    userExist.phone = phone;
+                    userExist.profileImage = profileImage;
+
+                    userExist.save().then(() => {
+                        res.status(201).json({ message: "profile updated" });
+                    })
+                        .catch(error => { res.status(500).json({ message: "error updating" }) })
+
+                }
+            })
+        }).catch(err => { console.log(err) });
+}
+
 
 const forgotPass = async (req, res) => {
     const { email } = req.body;
@@ -76,6 +196,7 @@ const verifyToken = async (req, res) => {
     }
 }
 
+
 const resetPassword = async (req, res) => {
     try {
         const token = req.params['token'];
@@ -99,4 +220,14 @@ const resetPassword = async (req, res) => {
 }
 
 
-module.exports = { forgotPass, verifyToken, resetPassword };
+module.exports = {
+    forgotPass,
+    verifyToken,
+    resetPassword,
+    deleteAcc,
+    loginHandler,
+    logoutHandler,
+    verifyPass,
+    editProfile,
+    signupHandler
+};
